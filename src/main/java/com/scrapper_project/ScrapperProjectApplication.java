045ce8dbe81cc637879f37212383306aeae84a7e
fedinairn08 <fedina_irn08@mail.ru>
2025-05-04 16:02:48 +0300

    Создан проект

---
+++ src/main/java/com/scrapper_project/ScrapperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.scrapper_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrapper_project/ScrapperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapper_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
