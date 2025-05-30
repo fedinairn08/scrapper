c183c244a86b1db5277934617cb586b54bbf7849
fedinairn08 <fedina_irn08@mail.ru>
2025-05-30 11:08:26 +0300

    В модуле link-parser написаны unit-тесты с примерами валидных и невалидных ссылок

--- link-parser/src/test/java/linkparser/linkparser/LinkParserApplicationTests.java
+++ link-parser/src/test/java/linkparser/linkparser/LinkParserApplicationTests.java
@@ -1,13 +1,57 @@
 package linkparser.linkparser;
 
+import linkparser.linkparser.model.GitHubResult;
+import linkparser.linkparser.model.StackOverflowResult;
+import linkparser.linkparser.service.LinkParseService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class LinkParserApplicationTests {
 
+	@Autowired
+	private LinkParseService linkParseService;
+
 	@Test
-	void contextLoads() {
+	public void parseValidGithubLink() throws URISyntaxException {
+		URI uri = new URI("https://github.com/fedinairn08/scrapper");
+
+		GitHubResult expected = new GitHubResult("fedinairn08", "scrapper");
+		GitHubResult actual = (GitHubResult) linkParseService.parseLink(uri);
+
+		assertNotNull(actual);
+		assertEquals(expected.user(), actual.user());
+		assertEquals(expected.repo(), actual.repo());
+    }
+
+	@Test
+	public void parseValidStackOverflowLink() throws URISyntaxException {
+		URI uri = new URI("https://stackoverflow.com/questions/927358/how-do-i-undo-the-most-recent-local-commits-in-git");
+
+		StackOverflowResult expected = new StackOverflowResult(927358);
+		StackOverflowResult actual = (StackOverflowResult) linkParseService.parseLink(uri);
+
+		assertNotNull(actual);
+		assertEquals(expected.questionId(), actual.questionId());
 	}
 
+	@Test
+	public void parseInvalidGithubLink() throws URISyntaxException {
+		URI uri = new URI("https://github.com/delete");
+
+		assertNull(linkParseService.parseLink(uri));
+	}
+
+	@Test
+	public void parseInvalidStackOverflowLink() throws URISyntaxException {
+		URI uri = new URI("https://stackoverflow.com/");
+
+		assertNull(linkParseService.parseLink(uri));
+	}
 }
