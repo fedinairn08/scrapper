707c8645efa6e97e8c077f1bbee391dcfd050aa5
fedinairn08 <fedina_irn08@mail.ru>
2025-06-22 11:00:50 +0300

    Тестирование сборки

--- scrapper/src/test/java/scrapper/scrapper/repository/jdbc/JdbcChatTests.java
+++ scrapper/src/test/java/scrapper/scrapper/repository/jdbc/JdbcChatTests.java
@@ -1,9 +1,11 @@
 package scrapper.scrapper.repository.jdbc;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import scrapper.scrapper.entity.Chat;
@@ -27,6 +29,15 @@ public class JdbcChatTests {
 
     @Autowired
     private ChatRepository chatRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    public void cleanDatabase() {
+        jdbcTemplate.update("DELETE FROM link");
+        jdbcTemplate.update("DELETE FROM chat");
+    }
 
     @BeforeAll
     public static void setTestChat() {
